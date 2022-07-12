f817816cb0720e4084e3d10996d2f0ca4ea63f7a
Sandro Mikautadze <sandromikautadze@yahoo.com>
2022-07-12 22:03:54 +0200

    Initial data cleaning and report update - Reduced the number of variables in the general and distric tibbles - Updated the report file with the new changes - Corrected careless mistake in README

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-#Quality of NYC Schools - Survey Analysis
+# Quality of NYC Schools - Survey Analysis
 
-# Introuction
+## Introuction
 
-# Research Question
+## Research Question
 
 - Do student, teacher and parent perceptions of NYC school quality appear to be related to demographic and academic success metrics? 
 
 - Do students, teachers, and parents have similar perceptions of NYC school quality?
 
-# Findings and Conclusions - Summary
+## Findings and Conclusions - Summary
 
-# Learning Objectives
+## Learning Objectives
 
-# Useful links
+## Useful links
 
 [Dataset](https://data.cityofnewyork.us/Education/2011-NYC-School-Survey/mnz3-dyi8) - last visited July 7th, 2022
--- src/00-data-processing.r
+++ src/00-data-processing.r
@@ -1,6 +1,16 @@
 library(tidyverse)
 
-# importing raw data
+# IMPORTING raw data
 combined <- read.csv("data/raw-data/combined.csv")
 general <- read_tsv("data/raw-data/masterfile11_gened_final.txt")
 district <- read_tsv("data/raw-data/masterfile11_d75_final.txt")
+
+# CLEANING 
+# keeping columns on aggregate survey responses on the 4 categories and removing
+# columns on number of respondents, rates, etc.
+general_reduced <- general %>%
+  select(1:32) %>% #keeping aggregate data columns
+  select(-starts_with("rr"), -starts_with("N"), -starts_with("nr")) #removing rate columns
+district_reduced <- district %>%
+  select(1:32) %>%
+  select(-starts_with("rr"), -starts_with("N"), -starts_with("nr"))
