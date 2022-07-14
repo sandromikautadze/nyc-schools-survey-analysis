ac10b277f776854e8af5f5fd5e5da85fba3bf543
Sandro Mikautadze <sandromikautadze@yahoo.com>
2022-07-14 17:04:50 +0200

    Finished data cleansing - Created final cleaned dataset - Saved cleaned dataset as "school-data.csv" in new folder called "clean-data" under "data" - Updated report and README

--- README.md
+++ README.md
@@ -15,4 +15,5 @@
 ## Useful links
 
 [Dataset 1](https://data.cityofnewyork.us/Education/2011-NYC-School-Survey/mnz3-dyi8) - last visited July 7th, 2022
+
 [Dataset 2](https://data.world/dataquest/nyc-schools-data/workspace/file?filename=combined.csv) - last visited Julty 13th, 2022
--- src/00-data-processing.r
+++ src/00-data-processing.r
@@ -9,13 +9,31 @@ district <- read_tsv("data/raw-data/masterfile11_d75_final.txt")
 general_reduced <- general %>%
   select(1:32) %>% #keeping aggregate data columns
   select(-starts_with("rr"), -starts_with("N"), -starts_with("nr")) %>% # removing rate columns
-  filter(schooltype %in% c("Middle / High School", "High School", "Elementary / Middle / High School")) # keeping high school rows
+  filter(schooltype == "High School") # keeping high school rows
 district_reduced <- district %>%
   select(1:32) %>%
-  select(-starts_with("rr"), -starts_with("N"), -starts_with("nr"))
+  select(-starts_with("rr"), -starts_with("N"), -starts_with("nr")) %>%
+  filter(studentssurveyed == "Yes")
 combined_reduced <- combined %>%
   select(-c(4:6)) #removing columns on specific SAT section scores
 
 # renaming column names lower case and underscore separated (just personal preference)
 colnames(combined_reduced) <- colnames(combined_reduced) %>% str_to_lower()
+
+# combining by rows general and district into a new data frame
+survey <- bind_rows(general_reduced, district_reduced)
+
+# removing columns after NA-value inspection
+survey <- survey %>% select(-highschool)
+combined_reduced <- combined_reduced %>% select(c(-number.of.exams.with.scores.3.4.or.5, high_score_percent))
+
+# joining data frames
+school_data_raw <- combined_reduced %>% left_join(survey, by = "dbn")
+# final cleaning
+school_data <- school_data_raw %>%
+  select(c(-bn, -schoolname, -schooltype, -studentssurveyed))
+
+#saving dataset in project folder
+write.csv(school_data, "~/nyc-schools-survey-analysis/data/clean-data/school-data.csv")
+
 
